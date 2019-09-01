7b44a4b95882be6715f9ed76c199219ab94b5249
Samir Saliba Jr <samirtsj@gmail.com>
2019-09-01 13:04:54 -0300

    Added reading from .csv files
    Now you can simply add the distances and heuristic between different cities straight from a csv file

--- astar.py
+++ astar.py
@@ -1,3 +1,14 @@
+# CEFET-MG BH, Eng. de Computacao
+# algoritmo A* (a estrela / astar) para busca de caminhos
+# para a disciplina Laboratorio de Inteligencia Artificial
+# professor Rogerio Martins
+# aluno: Samir Saliba Jr
+
+# algoritmo pode ser encontrado em https://github.com/samirsaliba/astar-pathfinding
+# foi baseado no pseudocodigo e na explicacao: https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+
+import csv
+
 class Vertex:
 	def __init__(self, node):
 		self.id = node
@@ -53,19 +64,21 @@ class Graph:
 
 	def add_edge(self, frm, to, distance=0):
 		if frm not in self.vertices:
-			print 'error adding edge: missing ' + frm + ' vertex'
-		elif to not in self.vertices:
-			print 'error adding edge: missing ' + to + ' vertex'
-		else:
-			self.vertices[frm].add_neighbor(self.vertices[to], distance)
-			self.vertices[to].add_neighbor(self.vertices[frm], distance)
+			self.add_vertex(frm)
+		if to not in self.vertices:
+			self.add_vertex(to)
 
+		if distance!=0:
+			self.vertices[frm].add_neighbor(self.vertices[to], distance)
+			self.vertices[to].add_neighbor(self.vertices[frm], distance)				
+			
 	def add_sld(self, frm, to, sld=0):
 		if frm not in self.vertices:
-			print 'error adding straight line distance: missing ' + frm + ' vertex'
-		elif to not in self.vertices:
-			print 'error adding straight line distance: missing ' + to + ' vertex'
-		else:
+			self.add_vertex(frm)
+		if to not in self.vertices:
+			self.add_vertex(to)
+
+		if sld!=0:
 			self.vertices[frm].add_sld(self.vertices[to], sld)
 			self.vertices[to].add_sld(self.vertices[frm], sld)
 
@@ -78,10 +91,6 @@ def aStar(grafo, str_start, str_end):
 
 	start = grafo.get_vertex(str_start)
 	end = grafo.get_vertex(str_end)
-
-	print 'A* (astar) pathfinding algorithm'
-	print 'from ' + start.id + ' to ' + end.id
-
 	
 	bound.append(start)
 
@@ -124,75 +133,26 @@ if __name__ == '__main__':
 
 	grafo = Graph()
 
-	grafo.add_vertex('oradea')
-	grafo.add_vertex('zerind')
-	grafo.add_vertex('arad')
-	grafo.add_vertex('timisoara')
-	grafo.add_vertex('lugoj')
-	grafo.add_vertex('mehadia')
-	grafo.add_vertex('dobreta')
-	grafo.add_vertex('craiova')
-	grafo.add_vertex('rimnicu')
-	grafo.add_vertex('sibiu')
-	grafo.add_vertex('fagaras')
-	grafo.add_vertex('pitesti')
-	grafo.add_vertex('bucharest')
-	grafo.add_vertex('giurgiu')
-	grafo.add_vertex('neamt')
-	grafo.add_vertex('iasi')
-	grafo.add_vertex('vaslui')
-	grafo.add_vertex('urziceni')
-	grafo.add_vertex('hirsova')
-	grafo.add_vertex('eforie')
+	with open('cidades.csv') as csvfile:
+		readCSV = csv.reader(csvfile, delimiter=',')
+		for row in readCSV:
+			print(row)
 
-	grafo.add_edge('oradea', 'zerind', 71)
-	grafo.add_edge('zerind', 'arad', 75)
-	grafo.add_edge('arad', 'timisoara', 118)
-	grafo.add_edge('timisoara', 'lugoj', 111)
-	grafo.add_edge('lugoj', 'mehadia', 70)
-	grafo.add_edge('oradea', 'sibiu', 151)
-	grafo.add_edge('mehadia', 'dobreta', 75)
-	grafo.add_edge('dobreta', 'craiova', 120)
-	grafo.add_edge('craiova', 'rimnicu', 146)
-	grafo.add_edge('craiova', 'pitesti', 138)
-	grafo.add_edge('rimnicu', 'sibiu', 80)
-	grafo.add_edge('arad', 'sibiu', 140)
-	grafo.add_edge('sibiu', 'fagaras', 99)
-	grafo.add_edge('fagaras', 'bucharest', 211)
-	grafo.add_edge('rimnicu', 'pitesti', 97)
-	grafo.add_edge('pitesti', 'bucharest', 101)
-	grafo.add_edge('giurgiu', 'bucharest', 90)
-	grafo.add_edge('bucharest', 'urziceni', 85)
-	grafo.add_edge('urziceni', 'hirsova', 98)
-	grafo.add_edge('hirsova', 'eforie', 86)
-	grafo.add_edge('urziceni', 'vaslui', 142)
-	grafo.add_edge('vaslui', 'iasi', 92)
-	grafo.add_edge('iasi', 'neamt', 87)
+			if row == []:
+				continue
+
+			elif row[0]=='e':
+				grafo.add_edge(row[1], row[2], int(row[3], 10))
+		
+			elif row[0]=='h':
+				grafo.add_sld(row[1], row[2], int(row[3], 10))
+
+			elif row[0]=='a*':
+				print 'astar: from ' + row[1] + ' to ' +  row[2]
+				start = row[1]
+				end = row[2]
 
 
-	grafo.add_sld('arad', 'bucharest', 366)
-	grafo.add_sld('bucharest', 'bucharest', 0)
-	grafo.add_sld('craiova', 'bucharest', 160)
-	grafo.add_sld('dobreta', 'bucharest', 242)
-	grafo.add_sld('eforie', 'bucharest', 161)
-	grafo.add_sld('fagaras', 'bucharest', 176)
-	grafo.add_sld('giurgiu', 'bucharest', 77)
-	grafo.add_sld('hirsova', 'bucharest', 151)
-	grafo.add_sld('iasi', 'bucharest', 226)
-	grafo.add_sld('lugoj', 'bucharest', 244)
-	grafo.add_sld('mehadia', 'bucharest', 241)
-	grafo.add_sld('neamt', 'bucharest', 234)
-	grafo.add_sld('oradea', 'bucharest', 380)
-	grafo.add_sld('pitesti', 'bucharest', 10)
-	grafo.add_sld('rimnicu', 'bucharest', 193)
-	grafo.add_sld('sibiu', 'bucharest', 253)
-	grafo.add_sld('timisoara', 'bucharest', 329)
-	grafo.add_sld('urziceni', 'bucharest', 80)
-	grafo.add_sld('vaslui', 'bucharest', 199)
-	grafo.add_sld('zerind', 'bucharest', 374)
-
-	path = aStar(grafo, 'lugoj', 'bucharest')
-
-
+	path = aStar(grafo, start, end)
 	for x in path:
 		print '-> ', x,
